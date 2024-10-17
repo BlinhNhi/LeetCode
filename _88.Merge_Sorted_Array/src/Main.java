6edbd20e672bf5dd779a7bed53f7fe3b34d28b2f
BlinhNhi <trantoan1682016@gmail.com>
2024-10-17 22:49:58 +0700

    up

--- _88.Merge_Sorted_Array/src/Main.java
+++ _88.Merge_Sorted_Array/src/Main.java
@@ -5,14 +5,16 @@ public class Main {
             m++;
         }
     }
-    //x=ai,a=n1
+    //x=ai = arr2 , a=n1 = arr1
     private static void chenPhanTuVaoMang(int x, int[] a, int m) {
         boolean timDuocK = false;
+        // for all element in arr[]1
         for (int k = 0; k < m ; k++) {
-            // phần tử của mảng n1 > phần tử n2
+            // phần tử của mảng n1 > phần tử n2(2 > 0)
             if(a[k] > x){
                 timDuocK = true;
-                // nếu số phần tử i trong arr1 > ptu i trong arr2
+                // nếu số phần tử i trong arr1 > ptu i trong arr2,
+                // m-1 vi dich i la so final xuong k chen len trc dc(4 chen len mat 5)
                 for(int i = m-1 ; i >= k ; i--){
                     // dịch phần tử > hơn sang phải
                     a[i+1] = a[i];
@@ -26,10 +28,9 @@ public class Main {
             a[m] = x;
         }
     }
-
     public static void main(String[] args) {
-        int[] n1={2,3,4,5,0,0,0};
-        int[] n2={0,3,6};
+        int[] n1={4,1,3,5,0,0,0};
+        int[] n2={4,0,6};
         merge(n1,4,n2,3);
         System.out.println("DONE");
     }

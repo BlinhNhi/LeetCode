358f36b7f929d6612a610c07bb73351757605934
BlinhNhi <trantoan1682016@gmail.com>
2024-10-29 00:01:08 +0700

    selection sort

--- Sort_Buble/src/Main.java
+++ Sort_Buble/src/Main.java
@@ -9,21 +9,21 @@ public class Main {
     public  static void bubbleSort(int[] a){
         int n = a.length;
         for (int i = 0; i < n; i++) {
-            boolean isSorted = true;
+//            boolean isSorted = true;
 
             //sau mỗi vòng lặp xét 2 ptu kề nhau(j , j+1 , đk: j+1 < n)
             // sau mỗi vòng lặp thì -i
             // (tìm ra ptử lớn nất phía cuối chỉ cần chạy đến i-1)
             for (int j = 0; j < n-i-1; j++) {
                 if(a[j] > a[j+1]){
-                    isSorted = false;
+//                    isSorted = false;
                     int t = a[j]; a[j]=a[j+1]; a[j+1]=t;
                 }
             }
             printArray(i,a);
-            if (isSorted){
-                break;
-            }
+//            if (isSorted){
+//                break;
+//            }
         }
     }
     public static void main(String[] args) {

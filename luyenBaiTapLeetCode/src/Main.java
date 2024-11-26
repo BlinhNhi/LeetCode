7edde8c5eb889c74eb1daab00682d159960cd215
BlinhNhi <trantoan1682016@gmail.com>
2024-11-26 15:01:40 +0700

    Luyện Bài Tập Mỗi Ngày

---
+++ luyenBaiTapLeetCode/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] a ={3,2,2,3};
+        System.out.println(removeElement(a,2));
+        System.out.println("DONE");
+    }
+    public static int removeElement(int[] a, int val) {
+      int n  = a.length;
+        for (int i = 0; i < n;) {
+            if(a[i] == val){
+                for (int j = i; j < (n-1); j++) {
+                    a[j] = a[j+1];
+                }
+                n--;
+            }else {
+                i++;
+            }
+        }
+        return  n;
+    }
+}

0420f69f02f7b8936becdb6feef91540f4a97d67
BlinhNhi <trantoan1682016@gmail.com>
2024-12-02 23:25:32 +0700

    update

--- luyenBaiTapLeetCode/src/Main.java
+++ luyenBaiTapLeetCode/src/Main.java
@@ -5,27 +5,15 @@ public class Main {
         System.out.println("DONE");
     }
     public static int removeElement(int[] a, int val) {
-        int n = 0;
+        int l = 0;
         for (int i = 0; i < a.length; i++) {
-            if(a[i] != val){
-                a[n] = a[i];
-                n++;
+            if(a[i] == val){
+                a[l] = a[i];
+                l++;
             }
         }
-        return n;
+        return l;
     }
+
 }
 
-
-//int n  = a.length;
-//        for (int i = 0; i < n;) {
-//        if(a[i] == val){
-//        for (int j = i; j < (n-1); j++) {
-//a[j] = a[j+1];
-//        }
-//n--;
-//        }else {
-//i++;
-//        }
-//        }
-//        return  n;

791fc9f71f2fdb2de4f1d882253d5b9621074678
BlinhNhi <trantoan1682016@gmail.com>
2025-02-21 23:46:26 +0700

    up

--- _27._Remove_Element/src/Main.java
+++ _27._Remove_Element/src/Main.java
@@ -6,16 +6,17 @@ public class Main {
     }
 
     public  static int  removeElement(int[] nums, int val) {
-        if(nums.length == 0){
-            return 0;
-        }
-        int valid_size = 0;
-        for(int i = 0 ; i<nums.length;i++){
-            if(nums[i] != val){
-                nums[valid_size] = nums[i];
-                valid_size ++;
+        int n = nums.length;
+        for(int i = 0 ; i < n ;){
+            if(nums[i] == val){
+                for(int j = i ; j < n-1 ;j++){
+                    nums[j] = nums[j+1];
+                }
+                n--;
+            }else{
+                i++;
             }
         }
-        return valid_size;
+        return n;
     }
 }

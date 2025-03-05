a27a6fd6f7a524743623f70db8acf1024422c666
BlinhNhi <trantoan1682016@gmail.com>
2025-03-05 23:22:04 +0700

    note 1672, 387

--- _1672_Richest_Customer_Wealth/src/Main.java
+++ _1672_Richest_Customer_Wealth/src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
+//    tìm tổng của hàng i : khách hàng
+//    tìm tổng của i nào lớn nhất. tài sản
     public static int maximumWealth(int[][] a) {
-        // i là khách hàng , j là ngân hàng
-    int soKhachHang = a.length;
-    int soNganHang = a[0].length;
+        // i là khách hàng : dòng , j là ngân hàng: cột
+    int soKhachHang = a.length; // số hàng
+    int soNganHang = a[0].length; // số cột a[0] là mảng 1 chiều => a.length sẽ là số cột
     int[] tongTaiSan = new int[soKhachHang];
         for (int i = 0; i < soKhachHang; i++) {
+//            tại mỗi i tính tất cả phần tử trong hàng i(tổng j)
             int tinhTongHangI = 0;
-            for (int j = 0; j < soNganHang; j++) {
+            for (int j = 0; j < soNganHang; j++) { // vòng lặp này để tính tổng tài sản của khách hàng(trả ra tổng all el j)
                 tinhTongHangI += a[i][j];
             }
-            tongTaiSan[i] = tinhTongHangI;
+            tongTaiSan[i] = tinhTongHangI; // gán lại cho phần tử i
         }
         int max=0;
         for (int i = 0; i < soKhachHang; i++) {
@@ -32,6 +35,7 @@ public class Main {
 //        int soHang = a.length; // 2 hàng
 //        int soCot = a[0].length; // 3 cột
 //
+//         // cách duyệt mảng 2 chiều.
 //        for (int i = 0; i < soHang; i++) {
 //            for (int j = 0; j < soCot; j++) {
 //                System.out.print(a[i][j]);
--- _387_First Unique Character in a String/src/Main.java
+++ _387_First Unique Character in a String/src/Main.java
@@ -1,21 +1,22 @@
 public class Main {
+//  tìm ký tự đầu tiên ko bị lặp lại trả về index nếu k có trả -1. Từ a - z : 97 - 122
     public static int firstUniqChar(String s) {
-        char[] arr = s.toCharArray();
-        int[] count = new int[123];
+        char[] arr = s.toCharArray(); //[l, o, v, e, l, e, e, d, c, o, d, e] // rải đoạn string ra từ ptu
+        int[] count = new int[123]; // tạo biến cout có 123 phần tử
+        // for này để tăng count của mỗi i lên
         for (int i = 0; i < arr.length; i++) {
-            char c = arr[i];
-            int index = (int)c;
-            count[index]++;
-
+            char c = arr[i]; //arr[0] = l = 108
+            int index = (int)c; // 108
+            count[index]++; // 108 có count là 1
         }
+//        for này để kiểm tra xem ele nào có count = 1 đầu tiên sẽ trả về vị trí đó
         for (int i = 0; i < arr.length; i++) {
-            char c = arr[i];
-            int index = (int)c;
+            char c = arr[i]; //arr[0] = l = 108
+            int index = (int)c; // 108
             if(count[index] == 1){
                 return i;
             }
         }
-
         return -1;
     }
     public static void main(String[] args) {

8fa4162dbae26be2ba8da5e3d195206fd31282f0
BlinhNhi <trantoan1682016@gmail.com>
2025-03-09 17:41:42 +0700

    up công thức tính fibo và giai thừa

--- Demo_DeQuy/src/Main.java
+++ Demo_DeQuy/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static  int GiaiThua(int n){
+//        Công thức tính giai thừa > 0 là n * !(n-1)
         // Bai Toan Co So
         if(n==0){
             return 1;
@@ -9,12 +10,12 @@ public class Main {
         return t;
     }
     public static void main(String[] args) {
-        System.out.println(GiaiThua(0));
-        System.out.println(GiaiThua(1));
+//        System.out.println(GiaiThua(0));
         System.out.println(GiaiThua(2));
-        System.out.println(GiaiThua(3));
-        System.out.println(GiaiThua(4));
-        System.out.println(GiaiThua(6));
+//        System.out.println(GiaiThua(2));
+//        System.out.println(GiaiThua(3));
+//        System.out.println(GiaiThua(4));
+//        System.out.println(GiaiThua(6));
 
     }
 }
--- Demo_DeQuy_Tree/src/Main.java
+++ Demo_DeQuy_Tree/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static  int Fibonanci(int n){
         System.out.println("Tính F : ["+n+"]");
-
+//        Công thức nếu n > 2 : F(n) = F(n-1) + F(n-2)=> F(4) = F(3) + F(2) => 2 + 1 = 3.
         if(n <= 2){
             return 1;
         }

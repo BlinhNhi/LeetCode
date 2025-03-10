e796cda900ad697c04e8f35c5603820b8ef645e3
BlinhNhi <trantoan1682016@gmail.com>
2025-03-10 18:06:29 +0700

    Khử đệ quy

--- Demo_DeQuy_Tree/src/Main.java
+++ Demo_DeQuy_Tree/src/Main.java
@@ -8,7 +8,46 @@ public class Main {
         int Fn = Fibonanci(n-1) + Fibonanci(n-2);
         return Fn;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    Dùng đệ qui có nhớ.
+    static  int [] F = new int[1000];
+    public static  int Fibonanci2(int n){
+//        Công thức nếu n > 2 : F(n) = F(n-1) + F(n-2)=> F(4) = F(3) + F(2) => 2 + 1 = 3.
+        if(F[n] !=0) return F[n];
+        System.out.println("Can tinh F["+n+"]");
+        if(n <= 2){
+            F[1] = 1;
+            F[2] = 1;
+            return 1;
+        }
+        F[n] = Fibonanci2(n-1) + Fibonanci2(n-2);
+        return F[n];
+    }
     public static void main(String[] args) {
         System.out.println(Fibonanci(5));
+//        System.out.println(Fibonanci2(5));
     }
 }

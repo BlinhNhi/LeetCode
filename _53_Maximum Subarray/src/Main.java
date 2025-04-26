0b7960b684d71115394a4bb709bb60fcf41c5ff0
BlinhNhi <trantoan1682016@gmail.com>
2025-04-26 18:26:25 +0700

    53. Maximum Subarray

---
+++ _53_Maximum Subarray/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+//        int[] nums={5,4,-1,7,8};
+//        int[] nums={-1,-2};
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+
+        maxSubArray(nums);
+        System.out.println("Done");
+    }
+
+    public static int maxSubArray(int[] nums) {
+       /* int length = nums.length;
+        int sum = 0;
+        int maxSum = nums[0];
+        for (int i = 0; i < length; i++) {
+            sum = sum + nums[i];
+            if(sum < 0){
+                sum = nums[i];
+            }else{
+                sum = sum + nums[i];
+            }
+
+            if(sum > maxSum){
+                maxSum = sum;
+            }
+        }
+        */
+        int length = nums.length;
+        int sum = 0;
+        int maxSum = nums[0];
+        for (int i = 0; i < length; i++) {
+            if (sum < 0) {
+                sum = nums[i];
+            } else {
+                sum = sum + nums[i];
+            }
+
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+        System.out.println(maxSum);
+        return maxSum;
+    }
+}
